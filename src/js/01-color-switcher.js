ffd912569a5061768ff973e57dcf9a64df2bd0c6
OleksandraSt <communication1@ukr.net>
2023-01-21 01:20:01 +0200

    додає дз 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
 
+startBtn.addEventListener('click', startColorSwitch);
+stopBtn.addEventListener('click', stopColorSwitch);
+
+stopBtn.setAttribute('disabled', true);
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function timerForColorSwitch() {
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function startColorSwitch() {
+    timerForColorSwitch();
+
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', true);
+}
+
+function stopColorSwitch() {
+    clearInterval(timerId);
+
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+}
